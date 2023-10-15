f032b232c0999bd1cd648179e0b784d8a6dcf96e
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-10-15 16:01:39 +0300

    making the html form and designing it
    made the html form and header and added the icons for the search and task

---
+++ index/main.js
@@ -0,0 +1,9 @@
+const addBtn = document.getElementById("add-task");
+addBtn.addEventListener('click', function(){
+    console.log("clicked");
+    validateForm();
+})
+function validateForm(){
+    console.log("validate");
+
+}
