11b306dde19721484b7af4f75ec067e0b26e659a
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-10-17 22:59:16 +0300

    completed the edit delet and complete button and their event handlers  and also made it work with local storage so it can store data even when we leave the site

--- index/main.js
+++ index/main.js
@@ -1,7 +1,10 @@
-import { validateForm, getValues, clear, createTask, renderTask} from './utility.js'
+import { validateForm, getValues, clear, setTaskField} from './utility.js'
 
 const addBtn = document.getElementById("add-task");
-const tasks=[];
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+let currentPosition = -1;
+
+renderTask(tasks);
 
 addBtn.addEventListener('click', function(){
     const isValidated = validateForm();
@@ -9,14 +12,130 @@ addBtn.addEventListener('click', function(){
         return
     const input = getValues()
     clear();
-    tasks.push(input);
+
+
+    if (addBtn.value === "Edit Task"){
+        tasks[currentPosition] = input
+        addBtn.value ="Add New Task";
+    }
+    else{
+
+        tasks.push(input);
+    }
     
-    console.log(tasks);
+    const stringify = JSON.stringify(tasks);
+    localStorage.setItem('tasks',stringify);
+
     renderTask(tasks);
     
-
-
 })
+
+
+
+function createTask(task, position) {
+    // console.log(task);
+     const tasksDiv = document.createElement("div");
+     tasksDiv.setAttribute("class", "tasks");
+ 
+     const imgGroupDiv = document.createElement("div");
+     imgGroupDiv.setAttribute("class", "img-group");
+ 
+     const image = document.createElement("img");
+     image.setAttribute("src", "image/icons8-task-96.png")
+     image.setAttribute("alt", "task icon");
+     image.setAttribute("class", "task-icon");
+ 
+     imgGroupDiv.appendChild(image);
+ 
+     const taskBodyDiv = document.createElement("div");
+     taskBodyDiv.setAttribute("class", "task-body");
+ 
+     const title = document.createElement("h4");
+     title.textContent = task.taskNameValue;
+ 
+     const paraDiv = document.createElement("div");
+     paraDiv.setAttribute("class", "para");
+ 
+     const dateTimePara = document.createElement("p");
+     dateTimePara.textContent = `${task.dateValue}: ${task.timeValue}`
+ 
+     const priorityPara = document.createElement("p");
+     priorityPara.textContent = task.priorityValue;
+     
+     const categoryPara = document.createElement("p");
+     categoryPara.textContent = task.categoryValue; 
+ 
+     const statusPara = document.createElement("p");
+     statusPara.textContent = `Status: ${task.statusValue}`; 
+ 
+     paraDiv.appendChild(dateTimePara);
+     paraDiv.appendChild(priorityPara);
+     paraDiv.appendChild(categoryPara);
+     paraDiv.appendChild(statusPara);
+ 
+     const btnGroupDiv = document.createElement("div");
+     btnGroupDiv.setAttribute("class", "btn-group");
+ 
+     const editBtn = document.createElement("button");
+     editBtn.textContent = "Edit";
+     editBtn.setAttribute("class", "btn-edit");
+ 
+     editBtn.addEventListener('click', function(){
+        
+        setTaskField(task);
+        currentPosition = position;
+     });
+ 
+     const deleteBtn = document.createElement("button");
+     deleteBtn.textContent = "Delete";
+     deleteBtn.setAttribute("class", "btn-delete");
+     
+     deleteBtn.addEventListener('click', function(){
+        
+        tasks.splice(position, 1);
+        renderTask(tasks)
+     })
+ 
+     const completedBtn = document.createElement("button");
+     completedBtn.textContent = "Completed";
+     completedBtn.setAttribute("class", "btn-comp");
+ 
+     completedBtn.addEventListener('click', function(){
+        task.statusValue = "Completed";
+        tasksDiv.style.backgroundColor = "green";
+        const stringify = JSON.stringify(tasks);
+        localStorage.setItem('tasks', stringify);
+      
+     })
+     if (task.statusValue=== "Completed") {
+        tasksDiv.style.backgroundColor = "green";
+    }
+ 
+     btnGroupDiv.appendChild(editBtn);
+     btnGroupDiv.appendChild(deleteBtn);
+     btnGroupDiv.appendChild(completedBtn);
+ 
+     taskBodyDiv.appendChild(title);
+     taskBodyDiv.appendChild(imgGroupDiv);
+     taskBodyDiv.appendChild(paraDiv)
+     taskBodyDiv.appendChild(btnGroupDiv);
+     
+     tasksDiv.appendChild(taskBodyDiv);
+ 
+     return tasksDiv;    
+ }
+ 
+ function renderTask(tasks) {
+     const render = document.querySelector('.task-group');
+     render.innerHTML = "";
+     for(let itemPosition = 0; itemPosition < tasks.length; itemPosition++){
+         const task = tasks[itemPosition]
+         const item = createTask(task,itemPosition);
+         render.appendChild(item,itemPosition);
+     }
+     
+ }
+ 
 
 
 
--- index/utility.js
+++ index/utility.js
@@ -3,6 +3,7 @@ const date = document.getElementById("date");
 const time = document.getElementById("time");
 const category = document.getElementById("category") ;
 const priority = document.getElementById("priority");
+const addBtn = document.querySelector("#add-task")
 
 //this finction validates the inputs of the form
 function validateForm(){
@@ -40,102 +41,29 @@ function validateForm(){
  }
  
  }
- 
-function createTask(task) {
-   // console.log(task);
-    const tasksDiv = document.createElement("div");
-    tasksDiv.setAttribute("class", "tasks");
 
-    const imgGroupDiv = document.createElement("div");
-    imgGroupDiv.setAttribute("class", "img-group");
+ function setTaskField(task) {
+    taskName.value = task.taskNameValue;
+    date.value = task.dateValue;
+    time.value = task.timeValue;
 
-    const image = document.createElement("img");
-    image.setAttribute("src", "image/icons8-task-96.png")
-    image.setAttribute("alt", "task icon");
-    image.setAttribute("class", "task-icon");
-
-    imgGroupDiv.appendChild(image);
-
-    const taskBodyDiv = document.createElement("div");
-    taskBodyDiv.setAttribute("class", "task-body");
-
-    const title = document.createElement("h4");
-    title.textContent = task.taskNameValue;
-
-    const paraDiv = document.createElement("div");
-    paraDiv.setAttribute("class", "para");
-
-    const dateTimePara = document.createElement("p");
-    dateTimePara.textContent = `${task.dateValue}: ${task.timeValue}`
-
-    const priorityPara = document.createElement("p");
-    priorityPara.textContent=task.priorityValue;
-    
-    const categoryPara = document.createElement("p");
-    categoryPara.textContent = task.categoryValue; 
-
-    const statusPara = document.createElement("p");
-    statusPara.textContent = `Status: ${task.statusValue}`; 
-
-    paraDiv.appendChild(dateTimePara);
-    paraDiv.appendChild(priorityPara);
-    paraDiv.appendChild(categoryPara);
-    paraDiv.appendChild(statusPara);
-
-    const btnGroupDiv = document.createElement("div");
-    btnGroupDiv.setAttribute("class", "btn-group");
-
-    const editBtn = document.createElement("button");
-    editBtn.textContent = "Edit";
-    editBtn.setAttribute("class", "btn-edit");
-
-    editBtn.addEventListener('click', function(){
-       
-        
-    });
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.setAttribute("class", "btn-delete");
-
-    deleteBtn.addEventListener('click', function(){
-       
-        
-    })
-
-    const completedBtn = document.createElement("button");
-    completedBtn.textContent = "Completed";
-    completedBtn.setAttribute("class", "btn-comp");
-
-    completedBtn.addEventListener('click', function(){
-     
-    })
-
-    btnGroupDiv.appendChild(editBtn);
-    btnGroupDiv.appendChild(deleteBtn);
-    btnGroupDiv.appendChild(completedBtn);
-
-    taskBodyDiv.appendChild(title);
-    taskBodyDiv.appendChild(imgGroupDiv);
-    taskBodyDiv.appendChild(paraDiv)
-    taskBodyDiv.appendChild(btnGroupDiv);
-    
-    tasksDiv.appendChild(taskBodyDiv);
-    
-    console.log(tasksDiv);
-
-    return tasksDiv;    
-}
-
-function renderTask(tasks) {
-    const render = document.querySelector('.task-group');
-    render.innerHTML = "";
-    for(let itemPosition = 0; itemPosition < tasks.length; itemPosition++){
-        const task = tasks[itemPosition]
-        const item = createTask(task);
-        render.appendChild(item);
+    for (let i = 0; i < category.options.length; i++) {
+        if (category.options[i].value === task.categoryValue) {
+            category.selectedIndex = i;
+            break;
+        }
     }
-    
-}
+
+    for (let i = 0; i < priority.options.length; i++) {
+        if (priority.options[i].value === task.priorityValue) {
+            priority.selectedIndex = i;
+            break;
+        }
+    }
+
+    addBtn.value = "Edit Task";
+ }
+ 
+
  export{
-    validateForm, getValues, clear, createTask, renderTask};
+    validateForm, getValues, clear, setTaskField};
